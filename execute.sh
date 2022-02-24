9f5ab7e0d113cdb2ada26f0beeabb913dd26bbf2
MikPhD <michele.quattromini@poliba.it>
2022-02-24 11:32:14 +0100

    slurm back

--- execute.sh
+++ execute.sh
@@ -11,5 +11,5 @@
 #SBATCH -o logs/case.out
 #SBATCH -e logs/case.err
 
-srun python Main.py -e 1 -r False -tcase 110 -vcas 110 -n_out 1
+srun python Main.py -e 100 -r False -tcase 110 -vcas 110 -n_out 100
 
