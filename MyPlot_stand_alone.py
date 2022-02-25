2485a31980c4d18ae051b9f8cd835f580cf06956
MikPhD <mik.quattromini@gmail.com>
2022-02-25 11:19:51 +0100

    set blank for slurm

--- MyPlot_stand_alone.py
+++ MyPlot_stand_alone.py
@@ -3,9 +3,11 @@ from MyPlot import Plot
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--n_epoch', help='epoch number', type=str, default="")
+parser.add_argument('-sn', '--set_name', help='parameter set name', type=str, default="30-09-0001-001")
 args = parser.parse_args()
 
 n_epoch = args.n_epoch
+set_name = args.set_name
 
-MyPlot = Plot()
+MyPlot = Plot(set_name)
 MyPlot.plot_results(n_epoch)
