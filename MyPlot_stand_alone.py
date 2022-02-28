a6db6398355865b0225c00975466ddb085890a36
MikPhD <mik.quattromini@gmail.com>
2022-02-28 17:05:41 +0100

    working on postprocess

--- MyPlot_stand_alone.py
+++ MyPlot_stand_alone.py
@@ -3,7 +3,7 @@ from MyPlot import Plot
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-e', '--n_epoch', help='epoch number', type=str, default="")
-parser.add_argument('-sn', '--set_name', help='parameter set name', type=str, default="30-09-0001-001")
+parser.add_argument('-sn', '--set_name', help='parameter set name', type=str, default="70-20-001-001")
 args = parser.parse_args()
 
 n_epoch = args.n_epoch
