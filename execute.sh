3d3093f34d19985dd4e0f35f30cf0723b7e3fa41
MikPhD <michele.quattromini@poliba.it>
2022-03-16 11:45:19 +0100

    slurm back

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -J v1_optuna
+#SBATCH -J v1_gru
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=64G
@@ -13,5 +13,5 @@
 #SBATCH --mail-user=mik.quattromini@gmail.com
 #SBATCH --mail-type=begin,end,fail
 
-srun python Main.py -e 1000 -r False -tcase 40 50 60 70 80 90 100 120 130 140 150 -vcas 110 -n_out 1000
+srun python Main.py -e 1000 -r True -tcase 40 50 60 70 80 90 100 120 130 140 150 -vcas 110 -n_out 160
 
