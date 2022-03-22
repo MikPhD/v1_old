29421b2fd06a96e19a7edb94c00ffabdfde88993
x-MQuatt <mik.quattromini@gmail.com>
2022-03-22 15:45:23 +0100

    update gru branch

--- MyTrain.py
+++ MyTrain.py
@@ -95,7 +95,7 @@ class Train_DSS:
                     l2_parameter[name] = params.grad.clone()
 
                 l3_parameter = {}
-                alpha = 0.9
+                alpha = 0.91
                 for key in l1_parameter:
                     l3_parameter[key] = (1-alpha) * l1_parameter[key] + (alpha) * l2_parameter[key]
 
