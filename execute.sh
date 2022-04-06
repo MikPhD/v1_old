36b26908507ef8e120947488d503d4edb5b89ace
MikPhD <mik.quattromini@gmail.com>
2022-04-06 15:34:44 +0200

    no self loop

--- execute.sh
+++ execute.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -J v1
+#SBATCH -J v1_noloop
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=64G
@@ -11,5 +11,5 @@
 #SBATCH -o logs/case.out
 #SBATCH -e logs/case.err
 
-srun python Main.py -e 1000000 -r False -tcase 40 50 60 70 80 90 100 120 130 140 150 -vcas 110 -n_out 5000
+srun python Main.py -e 1000 -r False -tcase 40 50 60 70 80 90 100 120 130 140 150 -vcas 110 -n_out 1000
 
